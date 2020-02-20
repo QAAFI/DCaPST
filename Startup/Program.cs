32f8be797b534a9d3226a675c61683e952407459
Michael Stower <m.stower@uq.edu.au>
2020-02-20 12:08:45 +1000

    Added startup project

---
+++ Startup/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DCAPST;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureServices();
+        }
+
+        public static IServiceProvider provider;
+
+        static void ConfigureServices()
+        {
+            provider = new ServiceCollection()
+                .AddSingleton()
+                .AddScoped()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ Utilities/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DCAPST.Canopy;
+using DCAPST.Environment;
+using DCAPST.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DCAPST
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSingleton(this IServiceCollection services)
+        {
+            services.AddSingleton<ITemperature, TemperatureModel>();
+            services.AddSingleton<ISolarRadiation, SolarRadiationModel>();
+            services.AddSingleton<ISolarGeometry, SolarGeometryModel>();            
+
+            return services;
+        }
+
+        public static IServiceCollection AddScoped(this IServiceCollection services)
+        {
+            services.AddScoped<ITotalCanopy, TotalCanopy>();
+
+            return services;
+        }
+    }
+}
